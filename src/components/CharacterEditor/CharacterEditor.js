7c3af4b3d5de031af02d554fe5eaa3af3e88340e
0815Strohhut <44547548+0815Strohhut@users.noreply.github.com>
2021-04-05 09:35:19 +0200

    Solution for the exercises

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,8 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div id="background" className={styles.coloredBackground}>
+      </div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -76,19 +78,20 @@ function App() {
             handleSelectOption={setClothesColor}
           />
         </div>
+        <div className={styles.characterWrapper}>
+          <Character
+            body={body}
+            head={head}
+            face={face}
+            accessory={accessory}
+            skinColor={skinColor}
+            clothesColor={clothesColor}
+          />
+        </div>
       </MaxWidthWrapper>
 
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
-    </main>
+
+    </main >
   );
 }
 
